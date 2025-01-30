5712867e5417c02bfe8fe64e96daf6d731da64de
Matteo Contrini <matteo@contrini.it>
2025-01-30 23:40:00 +0100

    Add underline to title

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -9,9 +9,12 @@
 </svelte:head>
 
 <div class="container mt-16 mb-20">
-	<h1 class="text-center text-6xl font-medium tracking-wide font-serif">
-		{data.tag.name}
-	</h1>
+	<div class="flex justify-center">
+		<h1 class="inline-block text-center text-6xl font-medium tracking-wide font-serif
+		      pb-2 px-1 border-b-4 border-b-violet-700 dark:border-b-violet-500">
+			{data.tag.name}
+		</h1>
+	</div>
 
 	<div class="mt-24">
 		<div class="text-4xl max-md:text-center">
