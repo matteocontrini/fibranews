3981fd9fb6902e7f7775928ac834c9a202e37f7d
Matteo Contrini <matteo@contrini.it>
2025-01-30 22:43:22 +0100

    Improve style

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -6,39 +6,49 @@
 	let showSources = $state(false);
 </script>
 
-<div>
-	<div class="uppercase font-medium bg-[#FDC400] text-yellow-900 inline-block px-2.5 py-0.5 rounded-sm">
-		{post.date}
-	</div>
-	<h3 class="text-3xl font-light mt-4 dark:text-white">
-		{post.title}
-	</h3>
+<div class="grid md:grid-cols-12 gap-y-6 max-md:border-b pb-10">
+	<div class="col-span-4">
+		<p class="text-slate-500 dark:text-slate-400 font-light text-xl leading-10">
+			{post.date}
+		</p>
 
-	<div class="mt-4 prose prose-lg dark:text-slate-300">
-		{@html post.content}
+		<!--		<div class="uppercase font-medium bg-[#FDC400] text-yellow-900 inline-block px-2.5 py-0.5 rounded-sm">-->
+		<!--			{post.date}-->
+		<!--		</div>-->
 	</div>
 
-	<div class="text-sm mt-6 text-slate-500 dark:text-slate-400">
-		{#if !showSources}
-			<button class="flex items-center gap-1.5 group" onclick={() => showSources = true}>
-				<span class="rotate-90">
-					»
-				</span>
-				<span class="group-hover:underline">
-					Mostra 5 fonti
-				</span>
-			</button>
-		{:else}
-			<ul>
-				<li class="flex items-center gap-2">
-					<img
-						src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://repubblica.it&size=32"
-						alt="" class="size-4">
-					<a class="text-sm text-slate-500 dark:text-slate-400 hover:underline" href="TODO">
-						Fastweb entra nel mercato dell’energia elettrica nel segno della sostenibilità
-					</a>
-				</li>
-			</ul>
-		{/if}
+	<div class="col-span-8">
+		<h3 class="text-4xl font-light dark:text-white">
+			{post.title}
+		</h3>
+
+		<div class="mt-6 prose prose-lg dark:text-slate-300 font-light">
+			{@html post.content}
+		</div>
+
+		<div class="text-sm mt-6 text-slate-500 dark:text-slate-400">
+			{#if !showSources}
+				<button class="flex items-center gap-1.5 group cursor-pointer"
+								onclick={() => showSources = true}>
+					<span class="rotate-90">
+						»
+					</span>
+					<span class="group-hover:underline">
+						Mostra 5 fonti
+					</span>
+				</button>
+			{:else}
+				<ul>
+					<li class="flex items-center gap-2">
+						<img
+							src="https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://repubblica.it&size=32"
+							alt="" class="size-4">
+						<a class="text-sm text-slate-500 dark:text-slate-400 hover:underline" href="TODO">
+							Fastweb entra nel mercato dell’energia elettrica nel segno della sostenibilità
+						</a>
+					</li>
+				</ul>
+			{/if}
+		</div>
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,25 +2,24 @@
 	import Post from '$lib/Post.svelte';
 
 	let { data } = $props();
-
-	// https://x.com/ilyamiskov/status/1881257760802967832/photo/1
 </script>
 
-<div class="w-[600px] mx-auto mt-12 mb-20">
-	<div class="flex justify-center items-center py-12 border rounded-sm bg-slate-50/40 dark:bg-slate-800/40 dark:border-slate-800">
-		<img src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Logo_Fastweb_2020.svg"
-				 class="w-[300px] mx-auto"
-				 alt="Fastweb">
-	</div>
-
-	<h2 class="mt-20 text-5xl font-bold text-center">
-		2024
+<div class="container mt-16 mb-20">
+	<h2 class="text-center text-5xl font-medium">
+		Fastweb
 	</h2>
 
-	<div class="mt-14 grid gap-14">
-		{#each data.posts as post}
-			<Post {post} />
-			<hr>
-		{/each}
+	<hr class="border-t my-20">
+
+	<div>
+		<div class="font-light text-4xl">
+			2025
+		</div>
+
+		<div class="mt-20 grid gap-16 md:gap-24">
+			{#each data.posts as post}
+				<Post {post} />
+			{/each}
+		</div>
 	</div>
 </div>
