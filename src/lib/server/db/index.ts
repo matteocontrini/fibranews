b938bd78a7c53bb5a0febc73e72edecadb9f627d
Matteo Contrini <matteo@contrini.it>
2025-02-01 11:29:40 +0100

    Enable typeorm sync and logs

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,12 +1,11 @@
 import { DataSource } from 'typeorm';
 import { PostEntity, SessionEntity, SourceEntity, TagEntity, UserEntity } from './schema';
-import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
 
 export const dataSource = new DataSource({
 	type: 'postgres',
 	url: env.DATABASE_URL,
 	entities: [PostEntity, TagEntity, SourceEntity, UserEntity, SessionEntity],
-	synchronize: dev,
-	logging: dev
+	synchronize: true,
+	logging: true
 });
