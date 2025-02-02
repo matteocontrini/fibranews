fd41968cb64b7bede455ff53a6badc21cf443f47
Matteo Contrini <matteo@contrini.it>
2025-02-02 09:25:28 +0100

    Prevent exposure of user data in ISR cache of hydration data

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,7 @@
 import { TagEntity } from '$lib/server/db/schema';
 import type { PopularTag } from '$lib/types';
 
-export async function load({ locals }) {
+export async function load({ locals, url }) {
 	const popularTags = await TagEntity.createQueryBuilder('tag')
 		.leftJoinAndSelect('tag.posts', 'posts')
 		.groupBy('tag.id, posts.id')
@@ -9,11 +9,17 @@ export async function load({ locals }) {
 		.limit(3)
 		.getMany();
 
+	// Expose the user only in admin otherwise it gets cached in the hydration data
+	const isAdminSection = url.pathname.startsWith('/admin');
+
 	return {
-		user: locals.user ? {
-			name: locals.user.name,
-			photoUrl: locals.user.photoUrl
-		} : null,
+		user:
+			isAdminSection && locals.user
+				? {
+						name: locals.user.name,
+						photoUrl: locals.user.photoUrl
+					}
+				: null,
 		popularTags: popularTags.map(mapPopularTag)
 	};
 }
